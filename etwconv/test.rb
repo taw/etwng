e575133fea820b473bb33452d976e40c479a8aea
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2010-10-10 22:29:28 +0100

    Infrastructure for testing and platform sanity checks

---
+++ etwconv/test.rb
@@ -0,0 +1,4 @@
+puts "Testing #{RUBY_PLATFORM}/#{RUBY_VERSION}"
+$: << "."
+
+require "test/platform_test"
---
+++ etwconv/test/platform_test.rb
@@ -0,0 +1,34 @@
+require "test/unit"
+
+# This test verifies that Ruby pack and unpack are compatible with assumptions used
+class TestPlatform < Test::Unit::TestCase
+  def assert_pack(fmt, data_ruby, data_bin)
+    assert_equal data_ruby, data_bin.unpack(fmt), "Unpacking #{fmt.inspect} should work"
+    assert_equal data_bin, data_ruby.pack(fmt),   "Packing #{fmt.inspect} should work"
+  end
+  
+  def test_byte
+    assert_pack "C", [  0], "\x00"
+    assert_pack "C", [  1], "\x01"
+    assert_pack "C", [127], "\x7F"
+    assert_pack "C", [128], "\x80"
+    assert_pack "C", [254], "\xFE"
+    assert_pack "C", [255], "\xFF"
+  end
+
+  def test_u4
+    assert_pack "V", [0x0000_0000], "\x00\x00\x00\x00"
+    assert_pack "V", [0x0000_0001], "\x01\x00\x00\x00"
+    assert_pack "V", [0x7FFF_FFFF], "\xFF\xFF\xFF\x7F"
+    assert_pack "V", [0x8000_0000], "\x00\x00\x00\x80"
+    assert_pack "V", [0xFFFF_FFFF], "\xFF\xFF\xFF\xFF"
+  end
+
+  def test_u2
+    assert_pack "v", [0x0000], "\x00\x00"
+    assert_pack "v", [0x0001], "\x01\x00"
+    assert_pack "v", [0x7FFF], "\xFF\x7F"
+    assert_pack "v", [0x8000], "\x00\x80"
+    assert_pack "v", [0xFFFF], "\xFF\xFF"
+  end
+end
