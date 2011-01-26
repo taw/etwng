d1cd7280559cccd99655f39a289435c9237dbbad
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2011-01-26 04:13:59 +0000

    ESF transformation scripts

---
+++ esfxml/scripts/esf_scripts.rb
@@ -0,0 +1,52 @@
+require "rubygems"
+require "nokogiri"
+
+class File
+  def self.write(path, content)
+    File.open(path, 'wb'){|fh|
+      fh.write(content)
+    }
+  end
+end
+
+class EsfScript
+  attr_reader :xmldir
+  
+  def initialize(xmldir)
+    raise "#{xmldir} doesn't exist" unless File.directory?(xmldir)
+    raise "#{xmldir} doesn't look like unpacked esf file" unless File.exist?(xmldir + "/esf.xml")
+    @xmldir = xmldir
+  end
+
+  def each_file(glob, &blk)
+    Dir[xmldir + "/" + glob].sort.select{|file_name| File.file?(file_name)}.each(&blk)
+  end
+  
+  def update_file(file_name)
+    content = File.open(file_name, 'rb', &:read)
+    new_content = yield(content)
+    if content == new_content
+      puts "No changes to #{file_name}"
+    else
+      puts "Updating #{file_name}"
+      File.write(file_name, new_content)
+    end
+  end
+
+  def update_each_xml(glob, xpath)
+    each_file(glob) do |file_name|
+      update_file(file_name) do |content|
+        doc = Nokogiri::XML.parse(content)
+        changed = false
+        doc.xpath(xpath).each do |elem|
+          changed = true if yield(elem)
+        end
+        if changed
+          doc.to_s
+        else
+          content
+        end
+      end
+    end
+  end
+end
